cf92d10852425437d26bee1baf575f68ebe80c53
shanekshimizu <shanekshimizu@gmail.com>
2020-10-12 21:36:28 -1000

    added comments

--- workordersbauto.py
+++ workordersbauto.py
@@ -8,92 +8,100 @@ import collections
 import time
 import re
 
+
 def translatepdf():
     """
-    use tabula to convert pdf file into .csv
+    Use tabula to convert pdf file into .csv
     """
-    list_of_files = glob.glob('/Users/shaneshimizu/Downloads/*') # * means all if need specific format then *.csv
+    list_of_files = glob.glob('/Users/shaneshimizu/Downloads/*')
     latest_file = max(list_of_files, key=os.path.getctime)
-    validatefile = input("name this file: ")
-    schoolcode = input("School Code: ")
-    print("\n")
-    #validatefile = input("Is " + latest_file + " the file you want to process?" + "\n type yes or no: ")
-
-    if validatefile.lower() != None:
-        dateRange = validatefile
-        #dateRange = input("Please enter the date range for this report (use underscore instead of spaces): ")
+    validateFile = input("name this file: ")
+    schoolCode = input("School Code: ")
+    
+    if validateFile.lower() != None:
+        dateRange = validateFile
+        #read recent file in download, read only set area, output into a csv file
         try:
-            data = read_pdf(latest_file, pages = 'all')
-            tabula.convert_into(latest_file, f'Work_Order_{dateRange}.csv',  guess=False, stream=True, area = (18.05,17.9,568.49,756.57), output_format="csv", pages = 'all')
+            data = read_pdf(latest_file, pages='all')
+            tabula.convert_into(latest_file, f'Work_Order_{dateRange}.csv',  guess=False, stream=True, area=(
+                18.05, 17.9, 568.49, 756.57), output_format="csv", pages='all')
+        #most recent file is not a accepted file for conversion
         except:
             print("not a fleet report, check recent download")
             return
-        workbookname = f'Work_Order_{dateRange}.csv'
+        workBookName = f'Work_Order_{dateRange}.csv'
         time.sleep(1)
-        executeAutomation(workbookname, schoolcode)
+        executeAutomation(workBookName, schoolCode)
     else:
         exit()
 
-def executeAutomation(workbookname, schoolcode):
-    with open(workbookname, 'r') as readit:
-        readfile = csv.reader(readit)
-        next(readfile)
-        with open(f'EXP_{workbookname}', 'w') as writeit, open(f'REV_{workbookname}', 'w') as writeit2:
-            writefile = csv.writer(writeit)
-            writefile2 = csv.writer(writeit2)
+def executeAutomation(workBookName, schoolCode):
+    """
+    Read the converted csv, extract account and respective charges only, write into two seperate csv files 
+    """
+    with open(workBookName, 'r') as readIt:
+        readFile = csv.reader(readIt)
+        next(readFile)
+        with open(f'EXP_{workBookName}', 'w') as writeExpData, open(f'REV_{workBookName}', 'w') as writeRevData:
+            writeExp = csv.writer(writeExpData)
+            writeRev = csv.writer(writeRevData)
             dicEachAccountTotals = {}
-            prices = dicEachAccountTotals.values()
-            total = sum(prices)
-            rowcount = 0
-            for column in readfile:
 
-                #should only use cells with these labels
-                hastotal = "Department Totals"
-                accountlbl = "Account"
+            for row in readFile:
+                # should only use cells with these labels
+                hasTotal = "Department Totals"
+                accountLabel = "Account"
 
-                #Add accounts to dictionary 
-                accountnum = column[0]
-                if accountnum[13:20] in dicEachAccountTotals:
-                    theaccount2 = accountnum[13:20]
-                    dicEachAccountTotals[str(theaccount2) + 'd'] = []
-                elif schoolcode.upper() in accountnum and accountlbl in accountnum:
-                    theaccount = accountnum[13:20]
-                    dicEachAccountTotals[theaccount] = [None]
+                # Add accounts to dictionary
+                accountNum = row[0]
+                if accountNum[13:20] in dicEachAccountTotals:
+                    theAccount = accountNum[13:20]
+                    dicEachAccountTotals[str(theAccount) + 'd'] = []
+                elif schoolCode.upper() in accountNum and accountLabel in accountNum:
+                    theAccount = accountNum[13:20]
+                    dicEachAccountTotals[theAccount] = [None]
 
-                #Add charges to each account in dictionary 
-                if hastotal in accountnum:
-                    priceselect = [6,7,8,9]
-                    pricenum = column[5]
-                    removecomma = pricenum.replace(',', '')
-                    allfloats = re.findall("[+-]?\d+\.\d+", removecomma)
+                # Add charges to each account in dictionary
+                if hasTotal in accountNum:
+                    price = row[5]
+                    priceSelect = [6, 7, 8, 9]  #other columns prices may be in
+                    
                     try:
-                        thetotal = float(allfloats[3])
+                        removeComma = price.replace(',', '')
+                        allFloats = re.findall("[+-]?\d+\.\d+", removeComma)
+                        realPrice = float(allFloats[3]) #grab the price at 3rd index of that row 
+                    #price is not in it's usual row
                     except IndexError:
-                        for i in priceselect:
+                        for i in priceSelect:
                             try:
-                                pricenum = column[i].replace(',', '')
-                                if pricenum == 0:
+                                price = row[i].replace(',', '')
+                                if price == 0: #price may sometimes read 0
                                     raise IndexError
                             except IndexError:
-                                if pricenum != 0:
-                                    removecomma = pricenum.replace(',', '')
-                                    allfloats = re.findall("[+-]?\d+\.\d+", removecomma)
-                                    thetotal = float(allfloats[-1])
+                                if price != 0:
+                                    removeComma = price.replace(',', '')
+                                    allFloats = re.findall("[+-]?\d+\.\d+", removeComma)
+                                    realPrice = float(allFloats[-1])
                                     break
-    
-                    recentkey = list(dicEachAccountTotals.keys())[-1]
-                    dicEachAccountTotals[f"{recentkey}"] = float(thetotal)
 
+                    #append the charge to the most recent account added to dictionary
+                    recentkey = list(dicEachAccountTotals.keys())[-1]
+                    dicEachAccountTotals[f"{recentkey}"] = float(realPrice)
+
+            #write accounts and charges to REV and EXP files
             for k, v in dicEachAccountTotals.items():
-                writefile.writerow((schoolcode, k[0:7], '', '5840', '', '', '', 'STATE VEHICLE R/M CHARGE', v))
-                writefile2.writerow(("MA", 2302698, '', '0751', '', '', '', 'STATE VEHICLE R/M CHARGE', v))
-                    
+                writeExp.writerow(
+                    (schoolCode, k[0:7], '', '5840', '', '', '', 'STATE VEHICLE R/M CHARGE', v))
+                writeRev.writerow(
+                    ("MA", 2302698, '', '0751', '', '', '', 'STATE VEHICLE R/M CHARGE', v))
+
         print("\n")
         pprint.pprint(collections.OrderedDict(dicEachAccountTotals))
 
-        os.system(f"open 'EXP_{workbookname}'")
-        os.system(f"open 'REV_{workbookname}'")
+        #open csv files to double check before submitting
+        os.system(f"open 'EXP_{workBookName}'")
+        os.system(f"open 'REV_{workBookName}'")
+
 
 if __name__ == "__main__":
     translatepdf()
-
