0a16fbb5b81b8c3c3925291abcb03ce5476784ad
shanekshimizu <shanekshimizu@gmail.com>
2020-12-17 19:18:19 -1000

    added save path

--- mailsbauto.py
+++ mailsbauto.py
@@ -5,39 +5,47 @@ import os
 from tabula import read_pdf
 import tabula
 import time
+import getpass
 
-
+username = getpass.getuser()
 def translatepdf():
     pass
     """
     use tabula to convert pdf file into .csv
     """
-    list_of_files = glob.glob('/Users/shaneshimizu/Downloads/*') # * means all if need specific format then *.csv
+    list_of_files = glob.glob(f'/Users/{username}/Downloads/*') # * means all if need specific format then *.csv
     latest_file = max(list_of_files, key=os.path.getctime)
     validatefile = input("Name file: ")
     schoolcode = input("School Code: ")
+    os.system(f"open '{latest_file}'")
 
     if validatefile.lower() != None:
         dateRange = validatefile
         try:
+            pathToFile = f'/Users/{username}/Desktop/service_billings/tabula_csv/'
+            fileName = f'Mail_{dateRange}.csv'
+            joinPath = os.path.join(pathToFile, fileName)
             data = read_pdf(latest_file, pages = 'all')
-            tabula.convert_into(latest_file, f'Mail_{dateRange}.csv', output_format="csv", pages = 'all')
+            tabula.convert_into(latest_file, joinPath, output_format="csv", pages = 'all')
         except:
             print("not a fleet report")
             return
-        workbookname = f'Mail_{dateRange}.csv'
+        workbookname = joinPath
         time.sleep(1)
-        executeAutomation(workbookname, schoolcode)
+        executeAutomation(workbookname, schoolcode, dateRange)
     else:
         exit()
 
 
-def executeAutomation(workbookname, schoolcode):
+def executeAutomation(workbookname, schoolcode, dateRange):
     #EXP and REV
     with open(workbookname, 'r') as readit:
         readfile = csv.reader(readit)
         next(readfile)
-        with open(f'EXP_{workbookname}', 'w') as writeit, open(f'REV_{workbookname}', 'w') as writeit2:
+        pathToFile = f'/Users/{username}/Desktop/service_billings/'
+        fileName = f'Mail_{dateRange}'
+        joinPath = os.path.join(pathToFile, fileName)
+        with open(f'{joinPath}_EXP.csv', 'w') as writeit, open(f'{joinPath}_REV.csv', 'w') as writeit2:
             writefile = csv.writer(writeit)
             writefile2 = csv.writer(writeit2)
             for column in readfile:
@@ -64,8 +72,8 @@ def executeAutomation(workbookname, schoolcode):
                 writefile.writerow((schoolcode, finalaccount,'', 3700, '', '', '', 'MAIL CHARGE', charge))
                 writefile2.writerow(('MA', 2214012, '', 3700, '', '', '', "MAIL CHARGE", charge))   
             
-    os.system(f"open 'EXP_{workbookname}'")
-    os.system(f"open 'REV_{workbookname}'")
+    os.system(f"open '{joinPath}_EXP.csv'")
+    os.system(f"open '{joinPath}_REV.csv'")
 
 if __name__ == "__main__":
     translatepdf()
